4c2650882363efd807fbdc22eb34a50b34be36a3
Yılmaz Demirci <yilmaz.demirci@orioninc.com>
2023-10-22 08:02:50 +0300

    app keys fix

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["9rPAwR5yNQ/0E2DEWU68qg==","pjyTp8RSjodcgQEzH95UQg==","qPPkxv+DNYinVeb5/rMpSw==","lPQVCP/Fcmq68wepaOGqPA=="],
   },
 });
