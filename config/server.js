0471f9dadf09698c5fcdb7f305d34debe07e9c14
Yılmaz Demirci <yilmaz.demirci@orioninc.com>
2023-10-22 08:17:46 +0300

    revert

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ["9rPAwR5yNQ/0E2DEWU68qg==","pjyTp8RSjodcgQEzH95UQg==","qPPkxv+DNYinVeb5/rMpSw==","lPQVCP/Fcmq68wepaOGqPA=="],
+    keys: env.array('APP_KEYS'),
   },
 });
